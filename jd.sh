32e93d5089d2126ac1addaaa2f5e2376106f5bb0
EvineDeng <evinedeng@foxmail.com>
2021-02-09 15:07:27 +0800

    Update

--- jd.sh
+++ jd.sh
@@ -74,9 +74,7 @@ function Combin_All {
   export JD_CASH_SHARECODES=$(Combin_Sub ForOtherCash)
   export JDNIAN_SHARECODES=$(Combin_Sub ForOtherNian)
   export JDNIANPK_SHARECODES=$(Combin_Sub ForOtherNianPk)
-  export JDSXSY_SHARECODES=$(Combin_Sub ForOtherImmortal)
   export JDSGMH_SHARECODES=$(Combin_Sub ForOtherSgmh)
-  export JSMOBILEFESTIVAL_SHARECODES=$(Combin_Sub ForOtherJdMobileFestival)
   export JD818_SHARECODES=$(Combin_Sub ForOtherJd818)
   export JDNY_SHARECODES=$(Combin_Sub ForOtherNY)
 }
