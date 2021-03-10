8d3eb3292e6b93c47d31030552d118453707aac4
Dellear <358009775@qq.com>
2021-03-10 11:59:38 +0800

    修复挂机日志超过1MB时，报执行出错的bug，调整为20MB限制

--- panel/server.js
+++ panel/server.js
@@ -493,9 +493,8 @@ app.post('/runCmd', function(request, response) {
     if (request.session.loggedin) {
         const cmd = request.body.cmd;
         const delay = request.body.delay || 0;
-        // console.log('before exec');
-        exec(cmd, (error, stdout, stderr) => {
-            // console.log(error, stdout, stderr);
+        // exec maxBuffer 20MB
+        exec(cmd, { maxBuffer: 1024 * 1024 * 20 }, (error, stdout, stderr) => {
             // 根据传入延时返回数据，有时太快会出问题
             setTimeout(() => {
                 if (error) {
